47cd3b5749f1ac42a6b066601b2bbb421c4d7358
f-ei8ht <saif7862254@outlook.com>
2025-01-02 16:44:46 +0530

    added: gitignore

--- First Class/First.java
+++ First Class/First.java
@@ -1,5 +1,3 @@
-import java.util.*; //importing a built in package
-
 public class First { // public is a access specifier and public class First we create a class 
     // if we add public before a class the class name and file name must be same along with case senstivity
     public static void main(String[] args) { // entry point for jvm
