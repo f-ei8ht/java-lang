0faddffb99da54cdd5d5c341695c5a07c084bc5d
f-ei8ht <saif7862254@outlook.com>
2025-01-17 00:15:10 +0530

    update basics.md

--- DataTypes_Variables_Input/Variables.java
+++ DataTypes_Variables_Input/Variables.java
@@ -1,18 +1,20 @@
 package DataTypes_Variables_Input;
 
-public class Variables{
+public class Variables {
     public static void main(String[] args) {
         int myNumber = 865; // Initialization
+        System.out.println(myNumber);
         int yourNumber = 0; // Declaration
         System.out.println(yourNumber); // no might not have been initialized
         yourNumber = 865; // assigning a value
         System.out.println(yourNumber);
-        yourNumber = 45; // 
+        yourNumber = 45; //
         System.out.println(yourNumber);
 
         float myfloat = 5.0f;
         double myDouble = 6.7;
         System.out.println(myfloat);
+        System.out.println(myDouble);
 
         boolean isVeg = false;
         System.out.println(isVeg);
@@ -22,8 +24,10 @@ public class Variables{
 
         char tot123 = 's';
         System.out.println(tot123);
-        
-        int _$_ = 1; // valid 
+
+        int _$_ = 1; // valid
+        System.out.println(_$_);
         // single quotes for character;
+
     }
 }
